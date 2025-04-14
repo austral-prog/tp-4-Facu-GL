22d52a43f9080195a588418d33cb83285d735ca6
Facu-GL <flgarcialorenzi@mail.austral.edu.ar>
2025-04-14 15:25:25 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,5 @@
 def leap_year():
-    print("TO DO")
-        year = int(input("Ingrese un año: "))
+    year = int(input("Ingrese un año: "))
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         print(f"El año {year} es bisiesto")
     else:
